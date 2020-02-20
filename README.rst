9594724cf50a0f42a10fd05e1c9c04d15ecdfb01
Peilonrayz <peilonrayz@gmail.com>
2020-02-20 16:24:53 +0000

    Normalize project structure

---
+++ README.rst
@@ -0,0 +1,40 @@
+envee
+=====
+
+.. image:: https://travis-ci.com/Peilonrayz/envee.svg?branch=master
+   :target: https://travis-ci.com/Peilonrayz/envee
+   :alt: Build Status
+
+About
+-----
+
+Easily manage multiple programming environments with ease.
+
+Installation
+------------
+
+.. code:: shell
+
+   $ python -m pip install envee
+
+Documentation
+-------------
+
+Documentation is available `via GitHub <https://peilonrayz.github.io/envee/>`_.
+
+Testing
+-------
+
+To run all tests run ``nox``. No venv is needed; nox makes all of them for us.
+
+.. code:: shell
+
+   $ python -m pip install --user nox
+   $ git clone https://peilonrayz.github.io/envee/
+   $ cd envee
+   envee $ nox
+
+License
+-------
+
+envee is available under the MIT license.
--- setup.py
+++ setup.py
@@ -1,44 +1,38 @@
 #!/usr/bin/env python
+from setuptools import find_packages, setup
 
-"""setup.py."""
-
-from setuptools import setup, find_packages
-
-with open('README.md', 'r') as f:
+with open("README.rst") as f:
     LONG_DESCRIPTION = f.read()
 
 setup(
-    name='envee',
-    version='0.0.0',
-    license='MIT',
-    description='Add tee like functionally to Popen',
+    name="envee",
+    version="0.0.0",
+    license="MIT",
+    description="Easily interact with virtual environments.",
     long_description=LONG_DESCRIPTION,
-    long_description_content_type="text/markdown",
-    author='Peilonrayz',
-    author_email='peilonrayz@gmail.com',
-    url='https://github.com/Peilonrayz/Teetime',
+    long_description_content_type="text/x-rst",
+    author="Peilonrayz",
+    author_email="peilonrayz@gmail.com",
+    url="https://peilonrayz.github.io/envee",
     project_urls={
-        'Source': 'https://github.com/Peilonrayz/Teetime',
-        'Tracker': 'https://github.com/Peilonrayz/Teetime/issues',
+        "Bug Tracker": "https://github.com/Peilonrayz/envee/issues",
+        "Documentation": "https://peilonrayz.github.io/envee",
+        "Source Code": "https://github.com/Peilonrayz/envee",
     },
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    keywords='tee popen command',
+    install_requires=['teetime'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    install_requires=[
-        'teetime',
-    ],
-    extras_require={
-        'dev':  [
-            'tox',
-        ]
-    },
+    keywords="",
+    # entry_points={"console_scripts": ["envee=envee.__main__:main"]},
 )
